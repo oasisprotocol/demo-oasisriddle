488396e8d92e4ace96000d79f68fb6a447c6af6d
Matevž Jekovec <matevz@oasisprotocol.org>
2023-11-20 11:41:30 +0100

    hardhat.config.ts: Use official Sapphire chain naming

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,21 +5,21 @@ import '@oasisprotocol/sapphire-hardhat';
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
-    'sapphire_mainnet': {
+    'sapphire': {
       url: 'https://sapphire.oasis.io',
       accounts: process.env.PRIVATE_KEY
         ? [process.env.PRIVATE_KEY]
         : [],
       chainId: 0x5afe
     },
-    'sapphire_testnet': {
+    'sapphire-testnet': {
       url: 'https://testnet.sapphire.oasis.dev',
       accounts: process.env.PRIVATE_KEY
         ? [process.env.PRIVATE_KEY]
         : [],
       chainId: 0x5aff
     },
-    'sapphire_localnet': {
+    'sapphire-localnet': {
       url: 'http://localhost:8545',
       accounts: process.env.PRIVATE_KEY
         ? [process.env.PRIVATE_KEY]
